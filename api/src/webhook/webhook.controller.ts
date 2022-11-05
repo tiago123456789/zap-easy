79c7c5d8f7dd203f81b71122fa2dbd1f4a47e93c
Tiago rosa da costa <tiagorosadacost@gmail.com>
2022-11-05 16:36:07 -0300

    Implemented the feature return webhooks created paginate

--- api/src/webhook/webhook.controller.ts
+++ api/src/webhook/webhook.controller.ts
@@ -10,57 +10,81 @@ import { CreatedWebhookDto } from './created-webhook.dto'
 @Controller("webhooks")
 export class WebhookController {
 
-    constructor(
-        private readonly webhookService: WebhookService
-    ) {}
+  constructor(
+    private readonly webhookService: WebhookService
+  ) { }
 
-    @ApiBearerAuth("TOKEN_JWT")
-    @ApiResponse({
-        status: 403,
-        description: "Action not allowed. The request needs send jwt token in request"
-    })
-    @ApiResponse({
-      status: 201,
-      type: CreatedWebhookDto,
-      description: "The webhook url to send text message via whatsapp is created"
-    })
-    @UseGuards(AuthorizationGuard)
-    @Post()
-    @HttpCode(201)
-    @UseGuards(AuthorizationGuard)
-    create(): Promise<CreatedWebhookDto> {
-        return this.webhookService.create();
-    }
+  @ApiBearerAuth("TOKEN_JWT")
+  @UseGuards(AuthorizationGuard)
+  @ApiResponse({
+    status: 403,
+    description: "Action not allowed. The request needs send jwt token in request"
+  })
+  @ApiResponse({
+    status: 200,
+    type: CreatedWebhookDto,
+    description: "The webhook created are returned"
+  })
+  @ApiParam({
+    name: "page",
+    description: "The page"
+  })
+  @ApiQuery({
+    name: "itemsPerPage",
+    description: "The total items return"
+  })
+  @Get("/")
+  findAll(@Query("page") page: number, @Query("itemsPerPage") itensPerPage: number) {
+    return this.webhookService.findAll(page, itensPerPage);
+  }
 
-    @ApiResponse({
-        status: 201,
-        description: "The request processed successfully"
-      })
-      @ApiResponse({
-        status: 404,
-        description: "The webhook url not found"
-      })
-      @ApiResponse({
-        status: 403,
-        description: "You try make request to the webhook url, but not specificied querystring <strong>key</strong> or <strong>key</strong> is invalid"
-      })
-      @ApiParam({
-        name: "id",
-        description: "The webhook id"
-      })
-      @ApiQuery({
-        name: "key",
-        description: "The <strong>key</strong> is used for validate if request sended to the third application is allow"
-      })
-    @Post("/:id")
-    @HttpCode(201)
-    notifyByWebhook(
-        @Param("id") id: string, 
-        @Query("key") key: string, 
-        @Body() message: MessageDto
-    ) {
-        return this.webhookService.notifyByWebhook(
-            id, key, message
-        )
-    }
+  @ApiBearerAuth("TOKEN_JWT")
+  @ApiResponse({
+    status: 403,
+    description: "Action not allowed. The request needs send jwt token in request"
+  })
+  @ApiResponse({
+    status: 201,
+    type: CreatedWebhookDto,
+    description: "The webhook url to send text message via whatsapp is created"
+  })
+  @UseGuards(AuthorizationGuard)
+  @Post()
+  @HttpCode(201)
+  @UseGuards(AuthorizationGuard)
+  create(): Promise<CreatedWebhookDto> {
+    return this.webhookService.create();
+  }
+
+  @ApiResponse({
+    status: 201,
+    description: "The request processed successfully"
+  })
+  @ApiResponse({
+    status: 404,
+    description: "The webhook url not found"
+  })
+  @ApiResponse({
+    status: 403,
+    description: "You try make request to the webhook url, but not specificied querystring <strong>key</strong> or <strong>key</strong> is invalid"
+  })
+  @ApiParam({
+    name: "id",
+    description: "The webhook id"
+  })
+  @ApiQuery({
+    name: "key",
+    description: "The <strong>key</strong> is used for validate if request sended to the third application is allow"
+  })
+  @Post("/:id")
+  @HttpCode(201)
+  notifyByWebhook(
+    @Param("id") id: string,
+    @Query("key") key: string,
+    @Body() message: MessageDto
+  ) {
+    return this.webhookService.notifyByWebhook(
+      id, key, message
+    )
+  }
 }
--- setup_rabbitmq.sh
+++ setup_rabbitmq.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-USER_PASSWORD="username_rabbitmq_here:password_rabbitmq_here"
+USER_PASSWORD="guest:guest"
 BASE_API_URL_RABBITMQ="http://localhost:15672"
 
 ############## CREATE EXCHANGES ################
@@ -38,10 +38,10 @@ curl -i -u $USER_PASSWORD -H "content-type:application/json" \
     $BASE_API_URL_RABBITMQ/api/exchanges/%2F/new_received_message_exchange_webhook_dlq
 
 
-############### CREATE QUEUES ##################
+# ############### CREATE QUEUES ##################
 
 curl -i -u $USER_PASSWORD -H "content-type:application/json" \
-    -XPUT -d'{"type":"direct","durable":true, "arguments":{"x-dead-letter-exchange":"update_status_instance_exchange_dlq", "x-dead-letter-routing-key": "update_status_routing_key_dlq"}}' \
+    -XPUT -d'{"durable":true,"arguments":{"x-dead-letter-exchange":"update_status_instance_exchange_dlq", "x-dead-letter-routing-key": "update_status_routing_key_dlq"}}' \
     $BASE_API_URL_RABBITMQ/api/queues/%2F/update_status_instance_queue
 
 
@@ -60,7 +60,7 @@ curl -i -u $USER_PASSWORD -H "content-type:application/json" \
     $BASE_API_URL_RABBITMQ/api/queues/%2F/received_message_queue_to_trigger_webhook
 
 
-############### CREATE DEAD LETTER QUEUES ##################
+# ############### CREATE DEAD LETTER QUEUES ##################
 
 curl -i -u $USER_PASSWORD -H "content-type:application/json" \
     -XPUT -d'{"type":"direct","durable":true }' \
@@ -82,7 +82,7 @@ curl -i -u $USER_PASSWORD -H "content-type:application/json" \
     $BASE_API_URL_RABBITMQ/api/queues/%2F/new_message_dlq
 
 
-#### BIND EXCHANGE WITH QUEUE #######
+### BIND EXCHANGE WITH QUEUE #######
 
 curl -i -u $USER_PASSWORD -H "content-type:application/json" \
     -XPOST -d'{"routing_key":"update_status_routing_key","arguments":{}}' \
@@ -110,7 +110,6 @@ curl -i -u $USER_PASSWORD -H "content-type:application/json" \
 curl -i -u $USER_PASSWORD -H "content-type:application/json" \
     -XPOST -d'{"routing_key":"new_received_message_websocket_routing_key_dlq","arguments":{}}' \
     $BASE_API_URL_RABBITMQ/api/bindings/%2f/e/new_received_message_exchange_websocket_dlq/q/received_message_queue_to_trigger_websocket_dlq
-
 
 curl -i -u $USER_PASSWORD -H "content-type:application/json" \
     -XPOST -d'{"routing_key":"new_received_message_webhook_routing_key_dlq","arguments":{}}' \
