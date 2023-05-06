4ce95e4bb852f6a7b1a3820959538dbe43c37512
Tiago rosa da costa <tiagorosadacost@gmail.com>
2023-05-06 01:09:02 -0300

    Implemented unit tests to the feature send text, image, audio or document message to specific instance

--- api/src/message/message.service.spec.ts
+++ api/src/message/message.service.spec.ts
@@ -1,7 +1,7 @@
 import { ProducerInterface } from "src/common/adapters/queue/producer.interface"
 import { RepositoryInterface } from "./adapters/repositories/repository.interface"
-import { RepositoryInterface as InstanceRepositoryInteface } 
-from "../instance/adapters/repositories/repository.interface"
+import { RepositoryInterface as InstanceRepositoryInteface }
+    from "../instance/adapters/repositories/repository.interface"
 
 import { Media } from "./entities/media.entity"
 import { Message } from "./entities/message.entity"
@@ -17,13 +17,19 @@ describe("MessageService", () => {
     let storage: jest.Mocked<StorageInterface>;
     let instanceRepository: jest.Mocked<InstanceRepositoryInteface<Instance>>;
     let instanceService: InstanceService;
+    const instance = new Instance();
+    instance.id = 'b5847af0-74ed-416a-a85c-c20a804031fa';
+    instance.isOnline = true;
+    instance.name = "Fake instance";
+    instance.updatedAt = new Date();
+    instance.createdAt = new Date();
 
     beforeEach(() => {
         instanceRepository = {
             save: jest.fn(),
             update: jest.fn(),
             findAll: jest.fn(),
-            findById: jest.fn() 
+            findById: jest.fn()
         };
         repository = {
             findOne: jest.fn(),
@@ -51,7 +57,7 @@ describe("MessageService", () => {
     })
 
     it("Should be send text message success", async () => {
-        
+
         const messageService = new MessageService(
             repository,
             mediaRepository,
@@ -70,9 +76,7 @@ describe("MessageService", () => {
         expect(mediaRepository.save).toBeCalledTimes(0)
     })
 
-    it("Should be send image message success", async () => {
-
-
+    it("Should be send text message to specific instance success", async () => {
         const messageService = new MessageService(
             repository,
             mediaRepository,
@@ -81,11 +85,32 @@ describe("MessageService", () => {
             instanceService
         )
 
+        instanceRepository.findById.mockResolvedValue(instance)
+        await messageService.send({
+            to: "556185615483",
+            text: "Fake message",
+            instanceId: instance.id
+        });
+
+        expect(repository.save).toBeCalledTimes(1)
+        expect(instanceRepository.findById).toBeCalledTimes(1)
+        expect(queueProducer.publish).toBeCalledTimes(1)
+        expect(mediaRepository.save).toBeCalledTimes(0)
+    })
+
+    it("Should be send image message success", async () => {
+        const messageService = new MessageService(
+            repository,
+            mediaRepository,
+            queueProducer,
+            storage,
+            instanceService
+        )
 
         await messageService.sendImage({
             to: "556185615483",
             text: "Fake message",
-            image: ""
+            image: "",
         });
 
         expect(repository.save).toBeCalledTimes(1)
@@ -94,10 +119,31 @@ describe("MessageService", () => {
         expect(storage.upload).toBeCalledTimes(1)
     })
 
+    it("Should be send image message to specific instance success", async () => {
+        const messageService = new MessageService(
+            repository,
+            mediaRepository,
+            queueProducer,
+            storage,
+            instanceService
+        )
+
+        instanceRepository.findById.mockResolvedValue(instance)
+        await messageService.sendImage({
+            to: "556185615483",
+            text: "Fake message",
+            image: "",
+            instanceId: instance.id
+        });
+
+        expect(repository.save).toBeCalledTimes(1)
+        expect(instanceRepository.findById).toBeCalledTimes(1)
+        expect(queueProducer.publish).toBeCalledTimes(1)
+        expect(mediaRepository.save).toBeCalledTimes(1)
+        expect(storage.upload).toBeCalledTimes(1)
+    })
 
     it("Should be send audio message success", async () => {
-
-
         const messageService = new MessageService(
             repository,
             mediaRepository,
@@ -109,7 +155,7 @@ describe("MessageService", () => {
         await messageService.sendAudio({
             to: "556185615483",
             text: "Fake message",
-            audio: ""
+            audio: "",
         });
 
         expect(repository.save).toBeCalledTimes(1)
@@ -118,9 +164,31 @@ describe("MessageService", () => {
         expect(storage.upload).toBeCalledTimes(1)
     })
 
+    it("Should be send audio message to specific instance success", async () => {
+        const messageService = new MessageService(
+            repository,
+            mediaRepository,
+            queueProducer,
+            storage,
+            instanceService
+        )
+
+        instanceRepository.findById.mockResolvedValue(instance)
+        await messageService.sendAudio({
+            to: "556185615483",
+            text: "Fake message",
+            audio: "",
+            instanceId: instance.id
+        });
+
+        expect(repository.save).toBeCalledTimes(1)
+        expect(instanceRepository.findById).toBeCalledTimes(1)
+        expect(queueProducer.publish).toBeCalledTimes(1)
+        expect(mediaRepository.save).toBeCalledTimes(1)
+        expect(storage.upload).toBeCalledTimes(1)
+    })
+
     it("Should be send document message success", async () => {
-
-
         const messageService = new MessageService(
             repository,
             mediaRepository,
@@ -142,6 +210,30 @@ describe("MessageService", () => {
         expect(storage.upload).toBeCalledTimes(1)
     })
 
+    it("Should be send document message to specific instance success", async () => {
+        const messageService = new MessageService(
+            repository,
+            mediaRepository,
+            queueProducer,
+            storage,
+            instanceService
+        )
+
+        instanceRepository.findById.mockResolvedValue(instance)
+        await messageService.sendDocument({
+            to: "556185615483",
+            text: "Fake message",
+            document: "",
+            instanceId: instance.id
+        });
+
+        expect(repository.save).toBeCalledTimes(1)
+        expect(instanceRepository.findById).toBeCalledTimes(1)
+        expect(queueProducer.publish).toBeCalledTimes(1)
+        expect(mediaRepository.save).toBeCalledTimes(1)
+        expect(storage.upload).toBeCalledTimes(1)
+    })
+
     it("Should be throw exception when try send more than 20 messages in batch", async () => {
         try {
             const messageService = new MessageService(
@@ -151,7 +243,7 @@ describe("MessageService", () => {
                 storage,
                 instanceService
             )
-    
+
             await messageService.sendTextMessagesInBatch({
                 messages: Array(30).fill({
                     to: "556185615483",
